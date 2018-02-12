2f14dd98dd82819e054bb6aa63d6807db068cb0f
Oliver van Porten <oliver@van-porten.de>
2018-02-12 21:51:41 +0100

    Workaround broken json sent from CCU ...

--- pyhomematic/_hm.py
+++ pyhomematic/_hm.py
@@ -272,7 +272,7 @@ class RPCFunctions(object):
             req = urllib.request.Request(apiendpoint, payload, headers)
             resp = urllib.request.urlopen(req)
             if resp.status == 200:
-                return json.loads(resp.read().decode('utf-8'))
+                return json.loads(resp.read().decode('utf-8').replace('$\\{','$\\\\{'))
             else:
                 LOG.error("RPCFunctions.jsonRpcPost: Status: %i" % resp.status)
                 return {'error': resp.status, 'result': {}}
